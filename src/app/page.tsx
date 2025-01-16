62fc89c2090d730a3ddec6c490330bc0d609429f
Shiv <shiv.7.shan@gmail.com>
2025-01-16 20:30:26 +0530

    show user page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import { Button } from "~/components/ui/button";
 import { auth } from "~/server/auth";
 
 export default async function Home() {
   const session = await auth();
 
+  if (session?.user?.name) {
+    redirect(`/${session.user.name}`);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center ">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[#faf9f7]">
       <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16">
         <h1 className="font-serif text-6xl font-bold tracking-tight text-gray-900 sm:text-7xl">
           ReadShelf
@@ -21,8 +26,8 @@ export default async function Home() {
           size="lg"
           className="mt-4 bg-red-800 hover:bg-[#723A10]"
         >
-          <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
-            {session ? "Sign out" : "Sign in"}
+          <Link href="/api/auth/signin">
+            Sign in
           </Link>
         </Button>
       </div>
